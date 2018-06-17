18730dbe8a2747233ac71ebc4feb57427d129f5e
Tomek (W101) <tbuslowski@gmail.com>
2018-06-17 21:00:40 +0200

    types of fromDate, toDate changed to timestamp

--- target/generated-sources/annotations/entities/Reservation_.java
+++ target/generated-sources/annotations/entities/Reservation_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-17T19:40:26")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-17T20:59:42")
 @StaticMetamodel(Reservation.class)
 public class Reservation_ { 
 
